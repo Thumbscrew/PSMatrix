e4940a7b45375d4e9459b930436f4dae80e23330
Thumbscrew <thumbscrw@pm.me>
2022-03-31 22:24:41 +0100

    add Get-Help section

--- public/Get-MatrixJoinedMembers.ps1
+++ public/Get-MatrixJoinedMembers.ps1
@@ -1,3 +1,22 @@
+<#
+    .Synopsis
+    Get all joined members for a given Room ID.
+
+    .Description
+    Get all joined members for a given Room ID from _matrix/client/v3/rooms/{roomId}/joined_members. See https://spec.matrix.org/v1.2/client-server-api/#get_matrixclientv3roomsroomidjoined_members.
+
+    .Parameter ServerUrl
+    URL for the Matrix server to log into, for example "https://matrix.example.com".
+
+    .Parameter AccessToken
+    A SecureString containing the Matrix access token.
+
+    .Parameter RoomId
+    The Matrix room ID to retrieve joined members for.
+
+    .Example
+    Get-MatrixJoinedMembers -ServerUrl $server -AccessToken $token -RoomId "!ehXvUhWNASUkSLvAGP:matrix.org"
+#>
 function Get-MatrixJoinedMembers {
     param(
         [Parameter(Mandatory)]
